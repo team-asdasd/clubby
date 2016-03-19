fc21d1f56532e8d9f03cdb508304ccad1b8b503d
Tony-Mc <maciulevicius.tony@gmail.com>
2016-03-19 17:52:44 +0200

    Created SecurityDataSource
    Should fix problems with freaking environment vars etc....

---
+++ src/main/java/security/shiro/SecurityDataSource.java
@@ -0,0 +1,17 @@
+package security.shiro;
+
+import org.postgresql.ds.PGSimpleDataSource;
+
+public class SecurityDataSource extends PGSimpleDataSource {
+    public SecurityDataSource() {
+        String username = System.getenv("OPENSHIFT_POSTGRESQL_DB_USERNAME");
+        String password = System.getenv("OPENSHIFT_POSTGRESQL_DB_PASSWORD");
+        String host = System.getenv("OPENSHIFT_POSTGRESQL_DB_HOST");
+        int port = Integer.parseInt(System.getenv("OPENSHIFT_POSTGRESQL_DB_PORT"));
+
+        super.setUser(username);
+        super.setPassword(password);
+        super.setServerName(host);
+        super.setPortNumber(port);
+    }
+}

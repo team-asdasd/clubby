38ef0d68b188f933fc70ed162f624eae13ce1c8e
Tony <maciulevicius.tony@gmail.com>
2016-03-24 10:28:19 +0200

    Update 04_roles_permissions.sql
    Removed redundant primary key statement. @rubilnikas

--- .sql/security/04_roles_permissions.sql
+++ .sql/security/04_roles_permissions.sql
@@ -13,11 +13,3 @@ WITH (
 );
 ALTER TABLE security.roles_permissions
   OWNER TO adminumaaqhz;
-
-
--- Constraint: security.roles_permissions_pkey
-
--- ALTER TABLE security.roles_permissions DROP CONSTRAINT roles_permissions_pkey;
-
-ALTER TABLE security.roles_permissions
-  ADD CONSTRAINT roles_permissions_pkey PRIMARY KEY(role_name);

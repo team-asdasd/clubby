3c29e2f7e307f423a193a95897a4fbc2815399f4
Gediminas Petrikas <gedas18@gmail.com>
2016-04-05 20:58:43 +0300

    Added bootstrap

--- src/main/webapp/src/vendor.ts
+++ src/main/webapp/src/vendor.ts
@@ -10,3 +10,6 @@ import 'rxjs';
 
 // Other vendors for example jQuery, Lodash or Bootstrap
 // You can import js, ts, css, sass, ...
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap/dist/fonts/glyphicons-halflings-regular.svg'

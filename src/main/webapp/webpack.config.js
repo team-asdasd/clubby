45fc044d37ef9c2cb6176349bd40837e3d7b0282
Gediminas Petrikas <gedas18@gmail.com>
2016-04-21 20:46:55 +0300

    Path fix

--- src/main/webapp/webpack.config.js
+++ src/main/webapp/webpack.config.js
@@ -60,7 +60,8 @@ module.exports = function makeWebpackConfig(){
      */
     config.output = isTest ? {} : {
         path: root('public/app'),
-        publicPath: isProd ? '/public/app' : 'http://localhost:3000/',
+        // publicPath: isProd ? '/public/app' : 'http://localhost:3000/',
+        publicPath: '/public/app',
         // publicPath: '/',
         filename: isProd ? 'js/[name].[hash].js' : 'js/[name].js',
         chunkFilename: isProd ? '[id].[hash].chunk.js' : '[id].chunk.js'

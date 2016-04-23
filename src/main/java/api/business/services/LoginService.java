242bace45bcc685a6931a5fcbf2adb34b8fc7493
Mindaugas Mažalskis <mindaugasmazalskis@gmail.com>
2016-04-23 21:28:09 +0300

    services methods

--- src/main/java/api/business/services/LoginService.java
+++ src/main/java/api/business/services/LoginService.java
@@ -3,13 +3,17 @@ package api.business.services;
 import api.business.entities.Login;
 import api.business.services.interfaces.ILoginService;
 
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 import javax.enterprise.context.RequestScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
+import javax.persistence.TypedQuery;
 
 @RequestScoped
 public class LoginService implements ILoginService {
-    @PersistenceContext
+    @PersistenceContext(type= PersistenceContextType.EXTENDED)
     private EntityManager em;
 
     public void createLogin(Login login) {
@@ -21,5 +25,16 @@ public class LoginService implements ILoginService {
         } catch (Exception e) {
             throw e;
         }
+    }
+
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public Login getByUserName(String username) {
+        try {
+            TypedQuery<Login> logins = em.createQuery("FROM logins WHERE username = :username", Login.class).setParameter("username", username);
+            return logins.getSingleResult();
+        }catch (Exception e) {
+            return null;
+        }
+
     }
 }

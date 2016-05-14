e02dfe511aabaef69a5b09647209a0c7f59f9044
Tony <maciulevicius.tony@gmail.com>
2016-05-14 19:57:41 +0300

    Cottage deletion

--- src/main/webapp/public/admin/shared/scripts/cottages.js
+++ src/main/webapp/public/admin/shared/scripts/cottages.js
@@ -1,36 +1,18 @@
-$.cloudinary.config({cloud_name: 'teamasdasd'})
+$.cloudinary.config({cloud_name: 'teamasdasd'});
 
-var alertDialog = window.Clubby.Alert();
+var dialogComponent = window.Clubby.Dialog();
+var alertComponent = window.Clubby.Alert();
 var dashboardMessage = $("#dashboard-message-box");
 
 $(function () {
-    var fileUpload = $("div[class^='uploadinput']").append($.cloudinary.unsigned_upload_tag("syjxlwur"));
-    fileUpload.children().first().attr("accept", "image/*");
-    fileUpload.bind('cloudinarydone', function (e, data) {
-            $("#image-upload").find("#progressbar").html("");
+    initializeFileUpload();
 
-            var image = $.cloudinary.image(data.result.public_id, {
-                crop: 'thumb', gravity: 'face', effect: 'saturation:50'
-            }).addClass("img-thumbnail img-responsive");
-
-            $('.preview').html(image);
-
-            $("#create-cottage-modal").find("#image").val(data.result.url);
-        }
-    );
-
-    fileUpload.bind('fileuploadprogress', function (e, data) {
-        var percent = Math.round((data.loaded * 100.0) / data.total);
-        var tpl = _.template('<div class="progress"><div class="progress-bar" role="progressbar" aria-valuenow="<%= percentage %>" aria-valuemin="0" aria-valuemax="100" style="width: <%= percentage %>%;"><%= percentage %>%</div></div>');
-        $("#image-upload").find("#progressbar").html(tpl({percentage: percent}));
-    });
-
-    $("#create-cottage-modal").find("#save").click(create);
+    $("#create-cottage-modal").find("#save").click(handleCreate);
 
     load();
-
 });
-function create() {
+
+function handleCreate() {
     var modal = $("#create-cottage-modal");
 
     var modalMessage = modal.find("#modal-message-box");
@@ -53,7 +35,7 @@ function create() {
         data: JSON.stringify(request)
     }).done(function () {
         modalMessage.html("");
-        dashboardMessage.html(alertDialog({title: "Success!", message: "Cottage created.", severity: "success"}));
+        dashboardMessage.html(alertComponent({title: "Success!", message: "Cottage created.", severity: "success"}));
 
         title.val("");
         bedcount.val("");
@@ -67,8 +49,42 @@ function create() {
         modalMessage.html("");
 
         var message = getErrorMessageFromResponse(response) || "Error creating cottage!";
-        modalMessage.html(alertDialog({title: "Error!", message: message, severity: "danger"}));
+        modalMessage.html(alertComponent({title: "Error!", message: message, severity: "danger"}));
     });
+}
+
+function handleDelete(event) {
+    var id = $(event.target).attr("data-cottage-id");
+
+    var dialog = $(dialogComponent({title: "Delete cottage?", body: "Are you sure you want to delete cottage?"}));
+
+    dialog.find(".dialog-yes").click(function () {
+        return $.ajax({
+            type: "DELETE",
+            url: "/api/cottage/" + id
+        }).done(function () {
+            dialog.modal("hide");
+
+            load().done(function () {
+                dashboardMessage.html(alertComponent({
+                    title: "Success!",
+                    message: "Cottage deleted.",
+                    severity: "success"
+                }));
+            });
+        }).fail(function (response) {
+            dialog.modal("hide");
+
+            var message = getErrorMessageFromResponse(response) || "Unknown error.";
+            dashboardMessage.html(alertComponent({title: "Error!", message: message, severity: "danger"}));
+        });
+    });
+
+    dialog.find(".dialog-no").click(function () {
+        dialog.modal("hide");
+    });
+
+    dialog.modal("show");
 }
 
 function load() {
@@ -76,18 +92,19 @@ function load() {
     var table = $("#cottages-table");
     table.html("<div class='loader'></div>");
 
-    $.ajax({
+    return $.ajax({
         type: "GET",
         url: "/api/cottage",
         contentType: "application/json; charset=utf-8",
         dataType: "json"
     }).done(function (response) {
         table.html(template(response));
+        $("#cottages-table").find(".remove-cottage").click(handleDelete);
     }).fail(function (response) {
         table.html("");
         var message = getErrorMessageFromResponse(response) || "Unknown error.";
 
-        dashboardMessage.html(alertDialog({title: "Error!", message: message, severity: "danger"}));
+        dashboardMessage.html(alertComponent({title: "Error!", message: message, severity: "danger"}));
     });
 }
 
@@ -105,3 +122,25 @@ function getErrorMessageFromResponse(response) {
     return message;
 }
 
+function initializeFileUpload() {
+    var fileUpload = $("div[class^='uploadinput']").append($.cloudinary.unsigned_upload_tag("syjxlwur"));
+    fileUpload.children().first().attr("accept", "image/*");
+    fileUpload.bind('cloudinarydone', function (e, data) {
+            $("#image-upload").find("#progressbar").html("");
+
+            var image = $.cloudinary.image(data.result.public_id, {
+                crop: 'thumb', gravity: 'face', effect: 'saturation:50'
+            }).addClass("img-thumbnail img-responsive");
+
+            $('.preview').html(image);
+
+            $("#create-cottage-modal").find("#image").val(data.result.url);
+        }
+    );
+
+    fileUpload.bind('fileuploadprogress', function (e, data) {
+        var percent = Math.round((data.loaded * 100.0) / data.total);
+        var tpl = _.template('<div class="progress"><div class="progress-bar" role="progressbar" aria-valuenow="<%= percentage %>" aria-valuemin="0" aria-valuemax="100" style="width: <%= percentage %>%;"><%= percentage %>%</div></div>');
+        $("#image-upload").find("#progressbar").html(tpl({percentage: percent}));
+    });
+}

bc4fad49c88bcffa6eadf5b57e3006ec252bc43d
elpaulas <elpaulas@gmail.com>
2016-05-14 18:59:42 +0300

    form page update

--- src/main/webapp/public/auth/scripts/form.js
+++ src/main/webapp/public/auth/scripts/form.js
@@ -1,29 +1,34 @@
 $.cloudinary.config({cloud_name: 'teamasdasd'})
-$("div[class^='uploadinput']").append($.cloudinary.unsigned_upload_tag("syjxlwur"))
-    .bind('cloudinarydone', function (e, data) {
-            $("div[class^='thumbnail']").append($.cloudinary.image(data.result.public_id,
-                {
-                    width: 100, height: 100,
-                    crop: 'thumb', gravity: 'face', radius: 'max'
-                }))
-            $('#photo').attr('value', data.result.url);
-            $('.progress').hide();
-        }
-    ).bind('fileuploadprogress', function (e, data) {
-    $('.progress').show();
-    $('.progress-bar').css('width', Math.round((data.loaded * 100.0) / data.total) + '%');
-});
-
+function insertPhotoField() {
+    $("div[class^='uploadinput']").append($.cloudinary.unsigned_upload_tag("syjxlwur"))
+        .bind('cloudinarydone', function (e, data) {
+                $('.thumbnail').append($.cloudinary.image(data.result.public_id,
+                    {
+                        width: 100, height: 100,
+                        crop: 'thumb', gravity: 'face', radius: 'max'
+                    }))
+                $('#photo').attr('value', data.result.url);
+                $('.photo-input').hide(200);
+            }
+        )
+}
 
 $.getJSON("/api/form", function (data) {
     for (var i = 0, len = data.fields.length; i < len; i++) {
         var field = data.fields[i];
+        var classType = "form-group";
+        if (field.required) classType = classType.concat(" required");
         if (field.name != 'photo')
-            $('<div>', {class: "col-xs-6 col-sm-6 col-md-6 form-group"}).append($('<input>').attr({
-                type: field.type, name: field.name, required: field.required,
-                placeholder: field.description, class: "form-control input-sm", pattern: field.validationRegex
-            })).prependTo('form')
-        else {
+            $('<div>', {class: classType}).append($('<label>').attr({
+                for: field.name,
+                class: "col-sm-2 control-label"
+            }).text(field.description)).append($('<div>').attr({class: 'col-sm-10'})
+                .append($('<input>').attr({
+                    type: field.type, name: field.name, class: "form-control", required: field.required,
+                    placeholder: field.description, pattern: field.validationRegex
+                }))).prependTo('form')
+        else if (field.visible) {
+            insertPhotoField();
             $('.photo').show();
         }
         $('.panel-body').show();
@@ -32,8 +37,13 @@ $.getJSON("/api/form", function (data) {
 })
 $('#submit').click(function () {
     var request = {fields: []};
+    var isValid = true
     $('input').each(function () {
         var input = $(this);
+        if(input.attr('required') != null && input.val() === ""){
+            isValid = false;
+            return
+        }
         if (input.attr('name') != 'file') {
             var field = {};
             field["name"] = input.attr('name');
@@ -41,6 +51,7 @@ $('#submit').click(function () {
             request.fields.push(field);
         }
     })
+    if(!isValid) return false;
     $.ajax({
         type: "POST",
         url: "/api/form",

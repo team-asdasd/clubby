5075de7e84f5b87ca49c6d2398ced1b08c0f3f85
Tony <maciulevicius.tony@gmail.com>
2016-05-22 20:09:13 +0300

    Default user picture

--- src/main/java/api/handlers/users/GetUserByIdHandler.java
+++ src/main/java/api/handlers/users/GetUserByIdHandler.java
@@ -1,6 +1,8 @@
 package api.handlers.users;
 
+import api.business.entities.Configuration;
 import api.business.entities.User;
+import api.business.persistance.ISimpleEntityManager;
 import api.business.services.interfaces.IFormService;
 import api.business.services.interfaces.IUserService;
 import api.contracts.base.ErrorCodes;
@@ -9,6 +11,7 @@ import api.contracts.users.GetUserByIdRequest;
 import api.contracts.users.GetUserInfoResponse;
 import api.handlers.base.BaseHandler;
 import api.helpers.Validator;
+import api.helpers.mappers.UserMapper;
 import clients.facebook.interfaces.IFacebookClient;
 import clients.facebook.responses.FacebookUserDetails;
 
@@ -20,22 +23,26 @@ import java.util.ArrayList;
 @Stateless
 public class GetUserByIdHandler extends BaseHandler<GetUserByIdRequest, GetUserInfoResponse> {
     @Inject
+    private ISimpleEntityManager em;
+    @Inject
     private IUserService userInfoService;
     @Inject
     private IFacebookClient facebookClient;
     @Inject
     private IFormService formService;
+    @Inject
+    private UserMapper mapper;
 
     @Override
     public ArrayList<ErrorDto> validate(GetUserByIdRequest request) {
-
-        ArrayList<ErrorDto> errors = Validator.checkAllNotNullAndIsAuthenticated(request);
-
-        return errors;
+        return Validator.checkAllNotNullAndIsAuthenticated(request);
     }
 
     @Override
     public GetUserInfoResponse handleBase(GetUserByIdRequest request) {
+        Configuration default_user_picture_url = em.getById(Configuration.class, "default_user_picture_url");
+        String defaultPic = default_user_picture_url != null ? default_user_picture_url.getValue() : "";
+        
         GetUserInfoResponse response = createResponse();
         User user = userInfoService.get(request.id);
 
@@ -44,21 +51,11 @@ public class GetUserByIdHandler extends BaseHandler<GetUserByIdRequest, GetUserI
             return handleException("User not found", ErrorCodes.NOT_FOUND);
         }
 
-        if (user.isFacebookUser()) {
-            try {
-                FacebookUserDetails userDetails = facebookClient.getUserDetailsById(user.getLogin().getFacebookId());
-                if (!userDetails.Picture.isSilhouette()) {
-                    response.picture = userDetails.Picture.getUrl();
-                }
-            } catch (IOException e) {
-                handleException(e);
-            }
-        }
-
-        response.fields = formService.getFormByUserId(request.id);
         response.id = user.getId();
-        response.email = user.getLogin().getEmail();
         response.name = user.getName();
+        response.email = user.getLogin().getEmail();
+        response.picture = mapper.getPicture(user, defaultPic);
+        response.fields = formService.getFormByUserId(request.id);
 
         return response;
     }
---
+++ src/main/java/api/helpers/mappers/UserMapper.java
@@ -0,0 +1,25 @@
+package api.helpers.mappers;
+
+import api.business.entities.User;
+import api.contracts.dto.UserDto;
+
+public class UserMapper {
+    public UserDto map(User u, String defaultPic) {
+        UserDto userDto = new UserDto(u);
+        userDto.picture = getPicture(u, defaultPic);
+        return userDto;
+    }
+
+    public String getPicture(User user, String defaultPic) {
+        if (user == null) {
+            throw new IllegalArgumentException("user");
+        }
+
+        String userPicture = user.getPicture();
+        if (userPicture != null && !userPicture.isEmpty()) {
+            return userPicture;
+        }
+
+        return defaultPic;
+    }
+}

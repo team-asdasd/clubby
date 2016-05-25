bf574c1c85ebb1c0a4fe41da931c3bb668498a27
Gediminas Petrikas <gedas18@gmail.com>
2016-05-25 20:01:49 +0300

    Formatted files

--- src/main/webapp/src/app/cottages/cottage-details/reservation-form/reservation-form.component.ts
+++ src/main/webapp/src/app/cottages/cottage-details/reservation-form/reservation-form.component.ts
@@ -1,6 +1,6 @@
 import {CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/common';
-
 import {Component} from 'angular2/core';
+
 import {Cottage} from '../../shared/cottage.model';
 import {RouteParams} from 'angular2/router';
 import {Reservation} from "../../shared/reservation.model";
@@ -9,7 +9,7 @@ import {ReservationService} from "../../shared/reservation.service";
 @Component({
     selector: 'reservation-form',
     template: require('./reservation-form.component.html'),
-    styles: [],
+    styles: [require('./reservation-form.component.scss')],
     providers: [ReservationService],
     directives: [CORE_DIRECTIVES, FORM_DIRECTIVES]
 })
@@ -22,7 +22,7 @@ export class ReservationForm {
                 private reservationService: ReservationService) {
         let id = this.routeParams.get('id');
         console.log(id);
-        this.reservation.CottageId = <number>id;
+        this.reservation.CottageId = Number(id);
     }
 
     onSubmit() {

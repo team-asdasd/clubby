d2a2604e5a21b2d03ecdaf419e782472a4bf3406
Mindaugas Mažalskis <mindaugasmazalskis@gmail.com>
2016-05-27 00:54:23 +0300

    config sql

--- .sql/02_main/08_configuration.sql
+++ .sql/02_main/08_configuration.sql
@@ -7,3 +7,4 @@ CREATE TABLE main.configuration (
 INSERT INTO main.configuration(key, value, description) VALUES ('min_recommendation_required', '2', 'Max recommendation requests.');
 INSERT INTO main.configuration(key, value, description) VALUES ('max_recommendation_request', '5', 'Min recommendation required to become member.');
 INSERT INTO main.configuration(key, value, description) VALUES ('default_user_picture_url', 'http://res.cloudinary.com/teamasdasd/image/upload/v1463936558/_default_avatar.jpg', 'Default picture for users without picture.');
+INSERT INTO main.configuration(key, value, description) VALUES ('groups_count', '4', 'Reservations groups count');

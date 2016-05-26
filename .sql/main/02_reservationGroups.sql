5ac90bcacc33390198928780430cfb81d9857a72
Mindaugas Mažalskis <mindaugasmazalskis@gmail.com>
2016-05-27 01:12:18 +0300

    alternative assignment service, all to first group

--- .sql/main/02_reservationGroups.sql
+++ .sql/main/02_reservationGroups.sql
@@ -5,5 +5,7 @@ CREATE TABLE main.reservationGroups
   generation INTEGER NOT NULL DEFAULT MAX(generation),
   groupNumber INTEGER NOT NULL,
   CONSTRAINT reservationGroups_pkey PRIMARY KEY (reservationGroupId),
+    CONSTRAINT fk_reservationGroups_users FOREIGN KEY (userId)
+  REFERENCES main.users (id) MATCH SIMPLE,
   unique (userid, generation)
 );
---
+++ src/main/java/api/business/services/FirstGroupAssignerService.java
@@ -0,0 +1,38 @@
+package api.business.services;
+
+import api.business.entities.Reservationgroup;
+import api.business.entities.User;
+import api.business.persistance.ISimpleEntityManager;
+import api.business.persistance.SimpleEntityManager;
+import api.business.services.interfaces.IGroupsAssignmentService;
+import api.contracts.reservations.groups.UserGroup;
+
+import javax.ejb.Stateless;
+import javax.enterprise.inject.Alternative;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Alternative
+public class FirstGroupAssignerService implements IGroupsAssignmentService {
+    @Inject
+    ISimpleEntityManager simpleEntityManager;
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public void Assign(List<User> users) {
+        int generationNumber = getLastGenerationNumber() +1;
+
+        simpleEntityManager.getAll(User.class).stream().forEach(f ->
+                simpleEntityManager.insert(new Reservationgroup(f.getId(), generationNumber, 1)));
+
+    }
+
+    private int getLastGenerationNumber(){
+        Integer max = (Integer)em.createNativeQuery("SELECT MAX(generation) FROM main.reservationgroups").getSingleResult();
+        return max == null ? 0 : max.intValue();
+    }
+}

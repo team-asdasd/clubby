77aa370ad1c21884a81d2e7a1c61f605c8f3403c
elpaulas <elpaulas@gmail.com>
2016-05-28 12:18:44 +0300

    get all visible fields when editing profile

--- src/main/java/api/contracts/dto/FormInfoDto.java
+++ src/main/java/api/contracts/dto/FormInfoDto.java
@@ -1,17 +1,28 @@
 package api.contracts.dto;
 
+import api.business.entities.Field;
 import api.business.entities.FormResult;
 
 public class FormInfoDto {
     public String description;
     public String type;
     public String value;
+    public String name;
 
-    public FormInfoDto(FormResult formResult){
+    public FormInfoDto(FormResult formResult) {
         this.description = formResult.getField().getDescription();
         this.type = formResult.getField().getType();
         this.value = formResult.getValue();
+        name = formResult.getField().getName();
     }
 
-    public FormInfoDto(){}
+    public FormInfoDto(Field field) {
+        this.description = field.getDescription();
+        this.type = field.getType();
+        value = "";
+        name = field.getName();
+    }
+
+    public FormInfoDto() {
+    }
 }

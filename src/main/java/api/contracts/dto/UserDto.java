63d6d00db8a15d0ed7a7c6a128a06e4c67ddd4d7
Tony <maciulevicius.tony@gmail.com>
2016-05-29 20:12:57 +0300

    Used inheritance to reduce duplications

--- src/main/java/api/contracts/dto/UserDto.java
+++ src/main/java/api/contracts/dto/UserDto.java
@@ -7,15 +7,7 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class UserDto {
-    public int id;
-    public String name;
-    public String email;
-    public boolean online;
-
-    @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
-    public String picture;
-
+public class UserDto extends SlimUserDto {
     @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
     public List<FormInfoDto> fields;
 
@@ -23,10 +15,7 @@ public class UserDto {
     public List<String> roles;
 
     public UserDto(User u) {
-        id = u.getId();
-        name = u.getName();
-        email = u.getLogin().getEmail();
-        picture = u.getPicture();
+        super(u);
 
         if (u.getFormResults() != null) {
             fields = u.getFormResults().stream().map(FormInfoDto::new).collect(Collectors.toList());
@@ -35,6 +24,5 @@ public class UserDto {
         if (u.getLogin().getRoles() != null) {
             roles = u.getLogin().getRoles().stream().map(Role::getRoleName).collect(Collectors.toList());
         }
-        online = u.isOnline();
     }
 }

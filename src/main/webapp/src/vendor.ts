f646cca15d47ad2d76787ee03a6c5dcb3ae761e3
Gediminas Petrikas <gedas18@gmail.com>
2016-05-30 21:44:32 +0300

    Fixed import

--- src/main/webapp/src/vendor.ts
+++ src/main/webapp/src/vendor.ts
@@ -5,7 +5,8 @@ import 'angular2/core';
 import 'angular2/router';
 import 'angular2/http';
 
-import 'angular2-notifications';
+import 'angular2-notifications/components.js';
+
 // RxJS
 import 'rxjs';
 

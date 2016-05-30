4f066855548ae4b2999f3df6c8a87f43d25b5070
Tony <maciulevicius.tony@gmail.com>
2016-05-30 23:10:06 +0300

    default fields inserts

--- .sql/02_main/04_form_fields.sql
+++ .sql/02_main/04_form_fields.sql
@@ -1,11 +1,18 @@
 CREATE TABLE main.form_fields
 (
-    id SERIAL PRIMARY KEY,
-    name TEXT,
-    type TEXT,
-    validation_regex TEXT,
-    required BOOLEAN DEFAULT FALSE ,
-    visible BOOLEAN,
-    description TEXT
+  id               SERIAL PRIMARY KEY,
+  name             TEXT,
+  type             TEXT,
+  validation_regex TEXT,
+  required         BOOLEAN DEFAULT FALSE,
+  visible          BOOLEAN,
+  description      TEXT
 );
 CREATE UNIQUE INDEX formfields_name_uindex ON main.form_fields (name);
+
+INSERT INTO main.form_fields (name, type, validation_regex, required, visible, description)
+VALUES ('phoneNumber', 'text', NULL, TRUE, TRUE, 'Phone number');
+INSERT INTO main.form_fields (name, type, validation_regex, required, visible, description)
+VALUES ('about', 'text', NULL, TRUE, TRUE, 'About');
+INSERT INTO main.form_fields (name, type, validation_regex, required, visible, description)
+VALUES ('birthDate', 'date', NULL, TRUE, TRUE, 'Birth date');

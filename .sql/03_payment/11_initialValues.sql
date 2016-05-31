a5408050f2e7b1214ea357a8854f27694463c46a
Mindaugas Mažalskis <mindaugasmazalskis@gmail.com>
2016-05-31 22:54:43 +0300

    buy 1k CB

--- .sql/03_payment/11_initialValues.sql
+++ .sql/03_payment/11_initialValues.sql
@@ -104,7 +104,7 @@ SELECT * FROM payment.payments
 WHERE paymentId = 1);
 
 INSERT INTO payment.payments
-SELECT * FROM (SELECT 2, 2,'EUR', 'Buy 100 Clubby coins', 1, true, false, 0) a
+SELECT * FROM (SELECT 2, 2,'EUR', 'Buy clubby coins', 1, true, false, 0) a
 WHERE NOT EXISTS (
 SELECT * FROM payment.payments
 WHERE paymentId = 2);
@@ -114,6 +114,12 @@ SELECT * FROM (SELECT 4, 3, 'EUR', '10 CB gift', 1, true, false, 0) a
 WHERE NOT EXISTS (
 SELECT * FROM payment.payments
 WHERE paymentId = 4);
+
+INSERT INTO payment.payments
+SELECT * FROM (SELECT 5, 2,'EUR', 'Buy clubby coins', 1, true, false, 0) a
+WHERE NOT EXISTS (
+SELECT * FROM payment.payments
+WHERE paymentId = 5);
 
 INSERT INTO payment.lineitems
 SELECT * FROM (SELECT nextval('lineitems_id_seq'),'Membership payment', 10000, 1, 1) a
@@ -132,3 +138,9 @@ SELECT * FROM (SELECT nextval('lineitems_id_seq'),'Gift', 1000, 1, 4) a
 WHERE NOT EXISTS (
 SELECT * FROM payment.lineitems
 WHERE payment_id = 4);
+
+INSERT INTO payment.lineitems
+SELECT * FROM (SELECT nextval('lineitems_id_seq'),'Buy clubby coins', 100000, 1, 5) a
+WHERE NOT EXISTS (
+SELECT * FROM payment.lineitems
+WHERE payment_id = 5);

5d85a40a34c3dcd7c0e007051967be07a8c95e1a
elpaulas <elpaulas@gmail.com>
2016-05-31 23:02:02 +0300

    log root cause

--- src/main/java/api/business/persistance/SimpleEntityManager.java
+++ src/main/java/api/business/persistance/SimpleEntityManager.java
@@ -1,5 +1,6 @@
 package api.business.persistance;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -7,8 +8,6 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.CriteriaQuery;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 @Stateless
@@ -19,57 +18,60 @@ public class SimpleEntityManager implements ISimpleEntityManager {
 
     private final Logger logger = LogManager.getLogger(getClass().getName());
 
-    public<T> T insert(T entity){
+    public <T> T insert(T entity) {
         try {
             em.persist(entity);
         } catch (Exception e) {
             em.clear();
-            logger.error(e);
+            logException(e);
             throw e;
         }
 
         return entity;
     }
 
-    public<T> T update(T entity){
+    public <T> T update(T entity) {
         T updated;
         try {
             updated = em.merge(entity);
             em.flush();
         } catch (Exception e) {
             em.clear();
-            logger.error(e);
+            logException(e);
             throw e;
         }
         return updated;
     }
 
-    public<T> void delete(T entity){
+    public <T> void delete(T entity) {
         try {
             em.remove(entity);
             em.flush();
         } catch (Exception e) {
             em.clear();
-            logger.error(e);
+            logException(e);
             throw e;
         }
     }
 
-    public<T> T getById(Class<T> entityClass, Object primaryKey){
+    public <T> T getById(Class<T> entityClass, Object primaryKey) {
         T entity = null;
-        try{
+        try {
             entity = em.find(entityClass, primaryKey);
-        }catch (Exception e){
-            logger.error(e);
+        } catch (Exception ignored) {
         }
 
         return entity;
     }
 
-    public<T> List<T> getAll(Class<T> entityClass){
+    public <T> List<T> getAll(Class<T> entityClass) {
         CriteriaQuery<T> criteria = em.getCriteriaBuilder().createQuery(entityClass);
         criteria.select(criteria.from(entityClass));
 
         return em.createQuery(criteria).getResultList();
+    }
+
+    private void logException(Exception e) {
+        logger.warn(ExceptionUtils.getRootCause(e));
     }
 }
